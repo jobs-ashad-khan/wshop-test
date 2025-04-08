db675dd6f21f9be934f2cc9a82d1afcbd89ca7da
Khan Ashad <jobs.ashad.khan@gmail.com>
2025-04-08 16:48:52 +0200

    Add bootsrap file and Refacto index.php

--- 3-API STORE/wshop-api-store/public/index.php
+++ 3-API STORE/wshop-api-store/public/index.php
@@ -3,57 +3,12 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use App\Kernel;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-
-Kernel::boot();
-
-$env = $_ENV['APP_ENV'] ?? 'dev';
-
-if ($env === 'dev') {
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
-} else {
-    ini_set('display_errors', 0);
-}
 
 $routes = require __DIR__ . '/../config/routes.php';
 
 $request = Request::createFromGlobals();
 
-$context = new RequestContext();
-$context->fromRequest($request);
-
-$matcher = new UrlMatcher($routes, $context);
-
-try {
-    $parameters = $matcher->match($request->getPathInfo());
-
-    $controllerInfo = $parameters['_controller'];
-    unset($parameters['_controller'], $parameters['_route']);
-
-    $controller = new $controllerInfo[0]();
-    $method = $controllerInfo[1];
-
-    $reflection = new \ReflectionMethod($controller, $method);
-    $params = $reflection->getParameters();
-
-    $paramTypes = array_map(fn($param) => $param->getType()?->getName(), $params);
-
-    if (in_array(Request::class, $paramTypes, true)) {
-        $parameters[] = $request;
-    }
-
-    $parameters = array_values($parameters);
-    $response = call_user_func_array([$controller, $method], $parameters);
-} catch (ResourceNotFoundException $e) {
-    $response = new JsonResponse(['message' => 'Data not found'], Response::HTTP_NOT_FOUND);
-} catch (Exception $e) {
-    $response = new JsonResponse(['message' => 'Internal Error'], Response::HTTP_INTERNAL_SERVER_ERROR);
-}
+$response = new Kernel()->handle($request);
 
 $response->send();
--- 3-API STORE/wshop-api-store/src/Kernel.php
+++ 3-API STORE/wshop-api-store/src/Kernel.php
@@ -2,13 +2,55 @@
 
 namespace App;
 
-use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 
 class Kernel
 {
-    public static function boot()
+    private $routes;
+
+    public function __construct()
     {
-        $dotenv = new Dotenv();
-        $dotenv->loadEnv(__DIR__.'/../.env');
+        $this->routes = require __DIR__ . '/../config/routes.php';
+    }
+
+    public function handle(Request $request)
+    {
+        $context = new RequestContext();
+        $context->fromRequest($request);
+
+        $matcher = new UrlMatcher($this->routes, $context);
+
+        try {
+            $parameters = $matcher->match($request->getPathInfo());
+
+            $controllerInfo = $parameters['_controller'];
+            unset($parameters['_controller'], $parameters['_route']);
+
+            $controller = new $controllerInfo[0]();
+            $method = $controllerInfo[1];
+
+            $reflection = new \ReflectionMethod($controller, $method);
+            $params = $reflection->getParameters();
+
+            $paramTypes = array_map(fn($param) => $param->getType()?->getName(), $params);
+
+            if (in_array(Request::class, $paramTypes, true)) {
+                $parameters[] = $request;
+            }
+
+            $parameters = array_values($parameters);
+            $response = call_user_func_array([$controller, $method], $parameters);
+        } catch (ResourceNotFoundException $e) {
+            $response = new JsonResponse(['message' => 'Data not found'], Response::HTTP_NOT_FOUND);
+        } catch (\Exception $e) {
+            $response = new JsonResponse(['message' => 'Internal Error'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return $response;
     }
 }
